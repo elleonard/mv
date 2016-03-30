2d6aad590ed7e8087965cb55a181cee9c43b891b
Saba <taniguchi03@gmail.com>
2016-03-30 18:13:59 +0900

    gulp

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -4,18 +4,18 @@ var replace = require('gulp-replace');
 var fs = require('fs');
 
 gulp.task('tachie', function(){
-  var file = '../Project/js/plugins/Saba_Tachie.js';
+  var file = '../js/Saba_Tachie.js';
   var stat = fs.statSync(file);
-  gulp.src(['../Project/js/plugins/Saba_Lib.js', file])
+  gulp.src(['../js/Saba_Lib.js', file])
     .pipe(concat('Saba_Tachie.js'))
     .pipe(replace('* Ver', '* Ver ' + getDateFormatString(stat.mtime)))
-    .pipe(gulp.dest('../dist/'));
+    .pipe(gulp.dest('../Project/js/plugins/'));
 });
 
 
 gulp.task('scenario', function(){
-  var file1 = '../Project/js/plugins/Saba_SimpleScenario.js';
-  var file2 = '../Project/js/plugins/Saba_SimpleScenarioValidator.js';
+  var file1 = '../js/Saba_SimpleScenario.js';
+  var file2 = '../js/Saba_SimpleScenarioValidator.js';
   var stat1 = fs.statSync(file1);
   var stat2 = fs.statSync(file2);
   
@@ -24,25 +24,25 @@ gulp.task('scenario', function(){
       mtime = stat2.mtime;
   }
   
-  gulp.src(['../Project/js/plugins/Saba_Lib.js', '../Project/js/plugins/Saba_SimpleScenario.js', '../Project/js/plugins/Saba_SimpleScenarioValidator.js'])
+  gulp.src(['../js/Saba_Lib.js', '../js/Saba_SimpleScenario.js', '../js/Saba_SimpleScenarioValidator.js'])
     .pipe(concat('Saba_SimpleScenario.js'))
     .pipe(replace('* Ver', '* Ver ' + getDateFormatString(mtime)))
-    .pipe(gulp.dest('../dist/'));
+    .pipe(gulp.dest('../Project/js/plugins/'));
 });
 
 
 gulp.task('backlog', function(){
-    var file = '../Project/js/plugins/Saba_BackLog.js';
+    var file = '../js/Saba_BackLog.js';
     var stat = fs.statSync(file);
-    gulp.src(['../Project/js/plugins/Saba_Lib.js', file])
+    gulp.src(['../js/Saba_Lib.js', file])
       .pipe(concat('Saba_BackLog.js'))
       .pipe(replace('* Ver', '* Ver ' + getDateFormatString(stat.mtime)))
-      .pipe(gulp.dest('../dist/'));
+      .pipe(gulp.dest('../Project/js/plugins/'));
 });
 
 
 gulp.task('watch', function(){
-  gulp.watch('../Project/js/plugins/*.js', ['tachie','scenario, backlog']);
+  gulp.watch('../js/*.js', ['tachie', 'scenario', 'backlog']);
 });
 
 gulp.task('default', ['watch']);
